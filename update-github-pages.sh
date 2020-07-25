288067c9c04064753cf467065334c8f2ff3a5446
Dylan McKay <me@dylanmckay.io>
2020-07-25 18:10:35 +1200

    Update the github pages deploy script so that it adds the expected CNAME file
    Before this patch, the deploy script would trash the CNAME file added on
    the repository settings page.

--- update-github-pages.sh
+++ update-github-pages.sh
@@ -4,6 +4,7 @@
 # artifacts to the GitHub 'gh-pages' branch.
 
 BOOK_DIR=$(dirname $0)
+TARGET_BRANCH=gh-pages
 
 function note() {
   echo "[note]: $@"
@@ -22,14 +23,19 @@ mdbook build -d "${staging_dir}" "${BOOK_DIR}"
 
 cd "${staging_dir}"
 git init
+
 git add .
 git commit -m "Generate the HTML version of the book"
-git branch -m gh-pages
+git branch -m $TARGET_BRANCH
+
+echo "book.avr-rust.com" > CNAME
+git add CNAME
+git commit -m "Add CNAME configuring the book.avr-rust.com custom domain"
 
 git remote add origin "${repository_url}"
 
-note "pushing the 'gh-pages' branch to '${repository_url}'"
+note "pushing the '$TARGET_BRANCH' branch to '${repository_url}'"
 
-git push origin gh-pages --force
+git push origin $TARGET_BRANCH --force
 
 note "staging directory left at '${staging_dir}'"
